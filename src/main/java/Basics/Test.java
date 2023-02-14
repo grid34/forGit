13c6ed6fc93591951cd340514237e66076c2d66e
grid34 <fremitage@gmail.com>
2023-02-14 14:39:32 -0600

    first commit

--- src/main/java/Basics/Test.java
+++ src/main/java/Basics/Test.java
@@ -6,6 +6,8 @@ public class Test {
 	public void help() {
 		System.out.println("hello");
 		System.out.println("after pushing");
+		
+		System.out.println("this is a test without pulling origin just commit in bach in firstbranch branch");
 	}
 	
 }
